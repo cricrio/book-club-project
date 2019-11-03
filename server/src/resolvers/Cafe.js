28a4036598e75ed58687558ff883efa44b257d5f
Christopher Bellanger <Christopher.Bellanger.p@gmail.com>
2019-11-03 15:28:23 +0100

    Fix bug in meetup query

--- server/src/resolvers/Cafe.js
+++ server/src/resolvers/Cafe.js
@@ -4,6 +4,8 @@ module.exports = {
   id: cafe => cafe._id,
   members: (cafe, __, { dataSources: { users } }) =>
     users.getUsersFromCafe(cafe._id),
-  meetups: async (cafe, __, { dataSources: { meetups } }) =>
-    groupPastAndFutureMeetups(await meetups.getMeetups(cafe._id))
+  meetups: async (cafe, __, { dataSources }) => {
+    const meetups = await dataSources.meetups.getMeetups(cafe._id);
+    return meetups.length ? groupPastAndFutureMeetups(meetups) : {};
+  }
 };
